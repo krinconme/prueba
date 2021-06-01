fbe9d345776e7456b0c08286cf9fb87cf7194900
krinconme <85189409+krinconme@users.noreply.github.com>
2021-06-01 12:58:02 -0500

    prueba
    prueba

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String thanks = "Thank you,";
+        String name = "miguel";
+        String order = "Your order number is #";
+        int previousOrder = 715;
+        System.out.print(thanks + " " + name.toUpperCase());
+        System.out.print(order + previousOrder);
+    }
+}
